a9c871be74b7f72fba1da0fd91ccffcc70267331
nisino25 <nisino25@gmail.com>
2022-11-26 17:39:07 +0900

    hey

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
+  <router-link to="/vsAI">AI</router-link>
   <div class="home">
+    
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
